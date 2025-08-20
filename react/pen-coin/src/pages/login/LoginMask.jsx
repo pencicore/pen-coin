b30bd8124b3f5a386bf11ee1152a292e332b3ec9
pencil_qbx <fu182929kangdao@163.com>
2025-08-20 10:14:55 +0800

    【前端】完成登录页-钱包登录窗口

--- react/pen-coin/src/pages/login/LoginMask.jsx
+++ react/pen-coin/src/pages/login/LoginMask.jsx
@@ -1,8 +1,13 @@
 import style from './LoginMask.module.scss'
+import {MiniGreenButton} from "../../components/Button.jsx";
 
 const LoginMask = () => {
     return (
-        <div className={style.LoginMask}></div>
+        <div className={style.LoginMask}>
+            <div className={style.MaskSvg}></div>
+            <p>{"检测到钱包登录"}</p>
+            <MiniGreenButton name={"连接"}></MiniGreenButton>
+        </div>
     )
 }
 
